96823af1fe4cc4d35f477b46d1732b22b88368ee
Stephen Zyszkiewicz <stevez@me.com>
2020-07-28 15:46:37 -0500

    add GRPC-vapor

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
-        .package(name:"GraphQLKit", url: "https://github.com/alexsteinerde/graphql-kit.git", .branch("2.0.0-beta.1"))
+        .package(name:"GraphQLKit", url: "https://github.com/alexsteinerde/graphql-kit.git", .branch("2.0.0-beta.1")),
+        .package(url: "https://github.com/Apodini/grpc-vapor.git", .branch("develop"))
     ],
     targets: [
         .target(
@@ -20,7 +21,8 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "GraphQLKit", package: "GraphQLKit")
+                .product(name: "GraphQLKit", package: "GraphQLKit"),
+                .product(name: "GRPCVapor", package: "grpc-vapor")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
